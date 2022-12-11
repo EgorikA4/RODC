7dcc5cec7500b7a10271e13e5edb807790a53cac
EgorikA4 <106007640+EgorikA4@users.noreply.github.com>
2022-12-11 19:31:46 +0300

    Delete rods_pr directory

--- rods_pr/test.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('qest.csv', newline='', mode='r', encoding='utf-8') as csvfile:
-    file = csv.DictReader(csvfile)
-    for row in file:
-        id_text = row['id']
-        full_text = row['text']
-        emotion = row['emotion']
