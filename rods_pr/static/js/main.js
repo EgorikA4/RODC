035e29fbf6dfec737fdaea3c7c45e5dbc899dc8d
EgorikA4 <egorik4litvinov@mail.ru>
2022-12-11 22:35:42 +0300

    alfa_1.0

---
+++ rods_pr/static/js/main.js
@@ -0,0 +1,89 @@
+const record = document.querySelector('.record');
+const time_audio = document.querySelector('.micro__text');
+
+const audio_block = document.querySelector('.micro__record-wrap');
+
+const success = document.querySelector('.success');
+
+const fail = document.querySelector('.fail');
+
+let audioCtx;
+
+var flag_control_audio = false;
+
+
+if (navigator.mediaDevices.getUserMedia) {
+  console.log('getUserMedia supported.');
+
+  const constraints = { audio: true };
+  let chunks = [];
+
+  let onSuccess = function(stream) {
+    const mediaRecorder = new MediaRecorder(stream);
+
+
+    record.onclick = function() { //Обработка записи
+      if(record.innerText == 'Записать'){
+        record.innerText = 'Стоп';
+        if(flag_control_audio){
+          const get_audio = document.querySelector('.audio_eg');
+          audio_block.removeChild(get_audio);
+          flag_control_audio = false;
+        }
+        mediaRecorder.start();
+        console.log(mediaRecorder.state);
+        console.log("recorder started");
+      }
+      else{
+        record.innerText = 'Записать';
+        mediaRecorder.stop();
+        console.log(mediaRecorder.state);
+        console.log("recorder stopped");
+      }
+    } //Обработка записи
+
+    mediaRecorder.onstop = function(e) {
+      console.log("data available after MediaRecorder.stop() called.");
+
+      const audio = document.createElement('audio');
+      audio.setAttribute('class', 'audio_eg');
+      const save_audio = document.createElement('a');
+
+      audio.controls = true;
+      const blob = new Blob(chunks, { 'type' : 'audio/wav' });
+      chunks = [];
+      const audioURL = window.URL.createObjectURL(blob);
+      audio.src = audioURL;
+      console.log("recorder stopped");
+
+      audio_block.appendChild(audio);
+      flag_control_audio = true;
+
+
+      success.onclick = function(){
+        save_audio.setAttribute('href', audioURL);
+        save_audio.setAttribute('download', 'test');
+        save_audio.click();
+        save_audio.removeAttribute('download');
+      }
+
+      fail.onclick = function(){
+        console.log('fail');
+      }
+
+    }
+
+    mediaRecorder.ondataavailable = function(e) {
+      chunks.push(e.data);
+    }
+  }
+
+  let onError = function(err) {
+    console.log('The following error occured: ' + err);
+  }
+
+  navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
+
+} else {
+   console.log('getUserMedia not supported on your browser!');
+}
